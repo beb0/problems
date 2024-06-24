392bdd8ff5d20d053723c426c3b7e39d66b734bb
n/a <n/a>
2024-06-25 00:28:33 +0300

    restructuring repo

--- CodeForces/1926C.cpp
+++ CodeForces/1926C.cpp
@@ -3,11 +3,31 @@ using namespace std;
 
 //https://codeforces.com/contest/1926/problem/C
 
-int sumofsum(int n)
+int sumofnum(int n)
 {
-    
+    if(n%10 == n)
+        return n;
+    else 
+        return n%10 + sumofnum(n/10);
 }
 
+
 int main() {
+
+    int n = 200002;
+    int arr[n];
+    
+    arr[0] = 0;
+
+    for (int i = 1; i < n; i++)
+        arr[i] = arr[i-1] + sumofnum(i);
+    
+    int tc; cin >> tc;
+
+    while(tc--)
+    {
+        int n; cin >> n;
+        cout << arr[n] << endl;
+    }
 
 }
