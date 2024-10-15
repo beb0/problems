433d7231c560627b88751e276498dc4081effbe7
beb0 <abd.diaa.g@gmail.com>
2024-10-15 13:24:46 +0300

    CF344A

---
+++ CodeForces/344A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, count = 1; cin >> n;
+    string arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin>> arr[i];
+
+    for (int i = 1; i < n; i++)
+    {
+       if(arr[i-1] != arr[i])
+        count++;
+    }
+
+    cout << count;
+    
+}
