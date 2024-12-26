bd7e85766211f3f2547977593eea02b9de8999ad
beb0 <abd.diaa.g@gmail.com>
2024-12-26 23:55:06 +0100

    CF766A

---
+++ CodeForces/766A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1, str2; cin >> str1 >> str2;
+
+    if(str1 == str2) cout << -1;
+    else cout << max(str1.size(), str2.size());
+}
